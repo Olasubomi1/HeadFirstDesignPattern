d6ed33237658acc6854c2e0164e2ab84214e7ba2
Olasubomi <shubbynlaa@gmail.com>
2023-02-10 02:49:28 +0100

    Explained the Strategy design pattern and how it was used to the Duck family class and its behavior.

--- src/Chapter1/MiniDuckSimulator.java
+++ src/Chapter1/MiniDuckSimulator.java
@@ -3,6 +3,16 @@ package Chapter1;
 import Chapter1.Implementations.FlyRocketPowered;
 
 public class MiniDuckSimulator {
+    /**
+     * The STRATEGY Pattern
+       --------------------
+     *The Strategy Pattern defines a family of algorithms,
+      encapsulates each one, and makes them interchangeable.
+      Strategy lets the algorithm vary independently of
+      clients that use it.
+     * The Strategy pattern was use in design the Duck family and its algorithm,
+       favoring composition over inheritance, and creating a system that's extensible.
+     * */
     public static void main(String[] args) {
         System.out.println("MALLARD DUCK");
         System.out.println("------------");
