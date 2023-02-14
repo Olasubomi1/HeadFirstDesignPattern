2f49791db2410e1e706b8ef707138ca98678cfdb
Olasubomi <shubbynlaa@gmail.com>
2023-02-14 06:17:54 +0100

    Added a new design principle.

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,7 @@ public class Main {
  * Identify the aspects of your application that vary and separate them from what stays the same.
  * Program to an interface, not an implementation.
  * Favor composition over inheritance.
+ * Strive for loosely coupled designs between objects that interact
+ *
  * */
 }
