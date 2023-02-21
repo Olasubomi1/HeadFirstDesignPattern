dbcd9cbc267ae899940904f409008c358d18197d
Olasubomi <shubbynlaa@gmail.com>
2023-02-21 01:24:05 +0100

    Created a weatherstation class to test the oberver pattern.

---
+++ src/Chapter2Observer/WeatherStation.java
@@ -0,0 +1,16 @@
+package Chapter2Observer;
+
+import Chapter2Observer.Observers.CurrentConditionsDisplay;
+import Chapter2Observer.Subject.WeatherData;
+
+public class WeatherStation {
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
+
+        weatherData.setMeasurements(80, 65, 30.4f);
+        weatherData.setMeasurements(82, 70, 29.2f);
+        weatherData.setMeasurements(78, 90, 29.2f);
+    }
+}
