836e7a385e6a9bcfb0a6298cdba967d3847cfb19
Olasubomi <shubbynlaa@gmail.com>
2023-02-27 18:54:23 +0100

    Created some other condiment decorator beverages.

---
+++ src/Chapter3Decorator/Soy.java
@@ -0,0 +1,17 @@
+package Chapter3Decorator;
+
+public class Soy extends CondimentDecorator{
+    public Soy(Beverage beverage){
+        this.beverage = beverage;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", Soy";
+    }
+
+    @Override
+    public double cost() {
+        return beverage.cost() + .50;
+    }
+}
