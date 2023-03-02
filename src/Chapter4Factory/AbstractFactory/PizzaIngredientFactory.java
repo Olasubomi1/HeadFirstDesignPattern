013125716d79a1f44372da7519bcee325faaa36b
Olasubomi <shubbynlaa@gmail.com>
2023-03-03 00:42:54 +0100

    Explaind the Abstract factory pattern.

--- src/Chapter4Factory/AbstractFactory/PizzaIngredientFactory.java
+++ src/Chapter4Factory/AbstractFactory/PizzaIngredientFactory.java
@@ -1,6 +1,13 @@
 package Chapter4Factory.AbstractFactory;
 
 public interface PizzaIngredientFactory{
+    /**
+     * ABSTRACT FACTORY
+     * ----------------
+     * The Abstract factory provides an interface for creating families of related
+     * or dependent objects without specifying their concrete classes.
+     *
+     */
     Dough createDough();
     Sauce createSauce();
     Cheese createCheese();
