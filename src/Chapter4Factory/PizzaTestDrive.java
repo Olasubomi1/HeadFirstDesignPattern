b10166f7ea43d00af83158136b2374e2d5832c80
Olasubomi <shubbynlaa@gmail.com>
2023-03-02 22:37:13 +0100

    Created test class to test the implemented factory method.

--- src/Chapter4Factory/PizzaTestDrive.java
+++ src/Chapter4Factory/PizzaTestDrive.java
@@ -1,2 +1,19 @@
-package Chapter4Factory;public class PizzaTestDrive {
+package Chapter4Factory;
+
+import Chapter4Factory.PizzaStoreType.ChicagoStylePizzaStore;
+import Chapter4Factory.PizzaStoreType.NYPizzaStore;
+
+public class PizzaTestDrive {
+
+    public static void main(String[] args) {
+        String pizzaType = "cheese";
+        PizzaStore nyStore = new NYPizzaStore();
+        PizzaStore chicagoStore = new ChicagoStylePizzaStore();
+
+        Pizza pizza = nyStore.orderPizza(pizzaType);
+        System.out.println("Ethan ordered a " + pizza.getName() + "\n");
+
+        pizza = chicagoStore.orderPizza(pizzaType);
+        System.out.println("Joel ordered a " + pizza.getName() + "\n");
+    }
 }
