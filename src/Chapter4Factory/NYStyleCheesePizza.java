94c924d3342cd230017244aaf391569859264f08
Olasubomi <shubbynlaa@gmail.com>
2023-03-02 22:31:43 +0100

    Created product classes which extended the abstract Pizza product class in the factory method pattern.

--- src/Chapter4Factory/NYStyleCheesePizza.java
+++ src/Chapter4Factory/NYStyleCheesePizza.java
@@ -1,2 +1,10 @@
-package Chapter4Factory;public class NYStyleCheesePizza {
+package Chapter4Factory;
+
+public class NYStyleCheesePizza extends Pizza{
+    public NYStyleCheesePizza(){
+        name = "NY Style Sauce and Cheese Pizza";
+        dough = "Thin Crust Dough";
+        sauce = "Marinara Sauce";
+        toppings.add("Grated Reggiano Cheese");
+    }
 }
