fb37350e3b59515e5427fa7faba13ac8cd875f60
Olasubomi <shubbynlaa@gmail.com>
2023-03-06 12:46:53 +0100

    defined the singleton pattern.

--- src/Chapter5Singleton/Singleton.java
+++ src/Chapter5Singleton/Singleton.java
@@ -1,6 +1,11 @@
 package Chapter5Singleton;
 
 public class Singleton {
+    /**
+     *THE SINGLETON PATTERN
+     *  The Singleton Pattern ensures a class has only one
+     * instance, and provides a global point of access to it.
+     */
     private static Singleton uniqueInstance;
     //Other useful instance variables here.
     private Singleton(){
